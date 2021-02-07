13afda1ded5b863bfdd5eafb4e400c4c87098b3d
Rasmus Møller <rasm38h8@stud.kea.dk>
2021-02-07 15:55:47 +0100

    template literals done

--- template.js
+++ template.js
@@ -1,25 +1,8 @@
 "use strict";
 
-let name1 = "Brian O'Conner";
-let name2 = "Rasmus \u{1f385}";
-
-const longline =
-  "a very very very very very very \n very very very long line that spans, \
- across the width of the editor very very very very very loooong";
-
-const name = "Peter";
-const drink = "Coca Cola";
-
-const text = `hello ${name}, would you 
-
-like a ${drink}?`;
-
-console.log(text);
-
-const navn = "Rasmus";
-const pet = "hamster";
-const petname = "Fluff";
+const navn = "Hagrid";
+const animalType = "dragon";
+const animalName = "Norbert";
 
 console.log(`My name is ${navn}.
-I have a ${pet} called ${petname}
-`);
+I have a ${pet} called ${petname}`);
