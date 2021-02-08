c24076d523c3f58bf0c6bb2f7cdd01aa9b2d4367
Rasmus Møller <rasm38h8@stud.kea.dk>
2021-02-08 11:58:18 +0100

    exercises done

---
+++ capitalization.js
@@ -0,0 +1,7 @@
+"use strict";
+
+const name = "Rasmusklahsdkfasjdknsjdkbsjks";
+const first = name.substring(0, 1).toUpperCase();
+const second = name.substring(1).toLowerCase();
+
+console.log(name);
---
+++ capitalization3.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const name = "Rasmus";
+const first = name.substring(0, 2);
+const second = name.substring(2, 3).toUpperCase();
+const third = name.substring(3, 6);
+const result = first + second + third;
+
+console.log(result);
