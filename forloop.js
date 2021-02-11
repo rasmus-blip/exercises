c91bbf426e35912fcda08dc98696e3f230246e3e
Rasmus Møller <rasm38h8@stud.kea.dk>
2021-02-11 19:51:11 +0100

    added the forloop files

---
+++ forloop.js
@@ -0,0 +1,43 @@
+"use strict";
+
+let counter;
+
+// // setting up, checks, adds
+// for (counter = 0; counter < 10; counter++) {
+//   console.log(counter);
+// }
+
+// console.log(`counter is now ${counter}`);
+
+// // setting up, checks, adds
+// for (counter = 1; counter < 11; counter++) {
+//   console.log(counter);
+// }
+// console.log(`counter is now ${counter}`);
+
+// // setting up, checks, adds
+// for (counter = 10; counter > 0; counter--) {
+//   console.log(counter);
+// }
+// console.log(`liftoff`);
+
+// setting up, checks, adds
+// for (counter = 1; counter < 20; counter += 2) {
+//   console.log(counter);
+// }
+
+// setting up, checks, adds
+// for (counter = 1; counter < 16777216; counter *= 2) {
+//   console.log(counter);
+// }
+// console.log(`liftoff`);
+
+// for (counter = 111; counter < 139; counter += 3) {
+//   console.log(counter);
+// }
+// console.log(`liftoff`);
+
+// for (counter = 100; counter > 0; counter -= 10) {
+//   console.log(counter);
+// }
+// console.log(`liftoff`);
